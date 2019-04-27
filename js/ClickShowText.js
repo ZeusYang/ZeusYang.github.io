43faa1def4ea79f475db0bcfddbc40ea19acc449
ZeusYang <1579148717@qq.com>
2019-04-27 11:49:28 +0800

    Site updated: 2019-04-27 11:49:27

---
+++ js/ClickShowText.js
@@ -0,0 +1,33 @@
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array
+        ("Computer Graphic", "C++", "OpenGL", "GLSL", "SYSU", "Qt", "Yangwc", "Modeling", "Rendering", "BRDF", "PBRT", "Ray Tracing");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 5,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "#00FF00"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+			3000,
+			function() {
+			    $i.remove();
+			});
+    });
+    setTimeout('delay()', 2000);
+});
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
